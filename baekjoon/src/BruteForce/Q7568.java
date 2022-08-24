57082f6cd835ec5f5ed0189c736e53c6e728c192
codingzzang123 <hosun9752@gmail.com>
2022-08-24 23:14:32 +0900

    feat: 2022-08-24 백준 알고리즘
    브루트포스 알고리즘과 자료구조 스택
    https://www.acmicpc.net/problem/7568
    https://www.acmicpc.net/problem/10828

--- baekjoon/src/BruteForce/Q7568.java
+++ baekjoon/src/BruteForce/Q7568.java
@@ -1,12 +1,52 @@
-package BruteForce;/**
- *packageName    : BruteForce
+package BruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * packageName    : BruteForce
  * fileName       : Q7568
  * author         : Hosun
  * date           : 2022-08-24
- * description    :
+ * description    : https://www.acmicpc.net/problem/7568
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2022-08-24        Hosun              최초 생성
- */public class Q7568 {
+ */
+
+public class Q7568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] weight = new int[n];
+        int[] height = new int[n];
+        int[] result = new int[n];
+
+        for(int i=0; i<n; i++){
+            String str = br.readLine();
+            StringTokenizer tokenizer = new StringTokenizer(str," ");
+            weight[i] = Integer.parseInt(tokenizer.nextToken());
+            height[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        for(int i=0; i<n; i++){
+            int k = 1;
+            for(int j=0; j<n; j++){
+                if(i == j)continue;
+
+                if(weight[i] < weight[j] && height[i] < height[j]){
+                    k++;
+                }
+            }
+            result[i] = k;
+        }
+
+        for(int i : result){
+            System.out.print(i+" ");
+        }
+    }
 }
--- baekjoon/src/Data_Structure/Stack_Q10828.java
+++ baekjoon/src/Data_Structure/Stack_Q10828.java
@@ -1,12 +1,89 @@
-package Data_Structure;/**
- *packageName    : Data_Structure
+package Data_Structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * packageName    : Data_Structure
  * fileName       : Stack_Q10828
  * author         : Hosun
  * date           : 2022-08-24
- * description    :
+ * description    : https://www.acmicpc.net/problem/10828
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2022-08-24        Hosun              최초 생성
- */public class Stack_Q10828 {
+ */
+public class Stack_Q10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<n; i++){
+            String temp = br.readLine();
+
+            if(temp.contains("push")){
+                StringTokenizer tokenizer = new StringTokenizer(temp," ");
+                tokenizer.nextToken();
+                stack.push(Integer.parseInt(tokenizer.nextToken()));
+            }else if (temp.equals("size")) {
+                sb.append(stack.size()).append("\n");
+            } else if (temp.equals("empty")) {
+                if(stack.isEmpty())
+                    sb.append(1).append("\n");
+                else
+                    sb.append(0).append("\n");
+            }else{
+                if(stack.isEmpty()){
+                    sb.append(-1).append("\n");
+                }else{
+                    if(temp.equals("pop")){
+                        sb.append(stack.pop()).append("\n");
+                    }
+                    if(temp.equals("top")){
+                        sb.append(stack.peek()).append("\n");
+                    }
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
 }
+
+/*
+    // sout을 하는 것 보다 StringBulider이용해서 하는 것이 더 빠르다.
+        for(int i=0; i<n; i++){
+            String temp = br.readLine();
+
+            if(temp.contains("push")){
+                StringTokenizer tokenizer = new StringTokenizer(temp," ");
+                tokenizer.nextToken();
+                stack.push(Integer.parseInt(tokenizer.nextToken()));
+            }else if (temp.equals("size")) {
+                System.out.println(stack.size());
+            } else if (temp.equals("empty")) {
+                if(stack.isEmpty())
+                    System.out.println(1);
+                else
+                    System.out.println(0);
+            }else{
+                if(stack.isEmpty()){
+                    System.out.println(-1);
+                }else{
+                    if(temp.equals("pop")){
+                        System.out.println(stack.pop());
+                    }
+                    if(temp.equals("top")){
+                        System.out.println(stack.peek());
+                    }
+                }
+            }
+        }
+*/
