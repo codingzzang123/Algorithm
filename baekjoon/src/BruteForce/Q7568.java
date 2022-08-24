ab09eb582dae70573c2d7cb792589564ba861bbd
codingzzang123 <hosun9752@gmail.com>
2022-08-24 22:52:33 +0900

    commit template

---
+++ baekjoon/src/BruteForce/Q7568.java
@@ -0,0 +1,12 @@
+package BruteForce;/**
+ *packageName    : BruteForce
+ * fileName       : Q7568
+ * author         : Hosun
+ * date           : 2022-08-24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-08-24        Hosun              최초 생성
+ */public class Q7568 {
+}
---
+++ baekjoon/src/Data_Structure/Stack_Q10828.java
@@ -0,0 +1,12 @@
+package Data_Structure;/**
+ *packageName    : Data_Structure
+ * fileName       : Stack_Q10828
+ * author         : Hosun
+ * date           : 2022-08-24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-08-24        Hosun              최초 생성
+ */public class Stack_Q10828 {
+}
